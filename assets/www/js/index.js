126dca9f154432fbf3e33cb16c66d44e8cfb1e46
PROPHESSOR <xenteze@gmail.com>
2017-02-10 13:12:30 +0200

    +gendoc

--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -78,3 +78,21 @@ function gotoURL(url){
 		window.location.href=url;
 	});
 }
+
+
+  function g_mail(){
+	var g_oper = document.getElementById('g_operator').value.split('(');
+	var g_about = document.getElementById('g_about').value;
+	var g_params = document.getElementById('g_params').value;
+	var g_return = document.getElementById('g_return').value;
+	var g_example = document.getElementById('g_example').value;
+	var oper = g_oper[0];
+	var args = "("+g_oper[1];
+	var params = g_params.split('\n').join('<br/>');
+	var tmp = "<oper>"+oper+"<args>"+args+"</args></oper><info>Описание:</info><block>"+g_about+"</block><info>Параметры:</info><block>"+params+"</block><info>Возвращаемое значение:</info><block>"+g_return+"</block><info>Пример использования:<block>"+g_example+"</block>";
+	document.getElementById('g_out').value = tmp;
+	log(tmp);
+	//document.write(tmp);
+
+	};
+	g_main();
